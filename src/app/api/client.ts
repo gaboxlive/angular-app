be307e5f6ee3c941f72e4992a8e42b0fafae2f3f
gaboxlive <gaboxlive@gmail.com>
2025-11-27 18:23:51 -0600

    actualizacion

---
+++ src/app/api/client.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Client {
+  protected http = inject(HttpClient);
+
+  getClientIp = (): Observable<any> => {
+    return this.http.get<any>(
+      'http://172.22.128.1:8000/api/v1/todos/clientIp'
+    );
+  };
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Client } from './api/client';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('angular-app');
+  clientApi = inject(Client);
+  public clientIp: string = '';
+
+  ngOnInit(): void {
+    this.getClientIp();
+  }
+
+  public getClientIp = () => {
+    this.clientApi.getClientIp().subscribe(response => {
+      this.clientIp = response.client_data;
+    })
+  }
+
 }
